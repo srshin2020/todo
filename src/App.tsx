6b795f73f6b3eac625ad5e4ad826d3aaf12cfe1a
sangrim shin <sangrimshin2020@gmail.com>
2026-02-18 22:31:20 +0900

    component 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 import { type ChangeEvent, type KeyboardEvent, useState } from 'react'
 import './App.css'
 
-function App() {
-  const [todo, setTodo] = useState('')
-  const [todos, setTodos] = useState<string[]>([])
 
-  const handleAddTodo = () => {
-    setTodos([...todos, todo])
-    setTodo('')
-  }
+function InputHeader({ onAddTodo, setTodo, todo }: { onAddTodo: () => void, setTodo: (todo: string) => void, todo: string }) {
 
   const handleChangeTodo = (e: ChangeEvent<HTMLInputElement>) => {
     setTodo(e.target.value)
@@ -16,22 +10,41 @@ function App() {
 
   const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
-      handleAddTodo()
+      onAddTodo()
     }
   }
+  return (
+    <div className="todo-container">
+      <input className="todo-input" onKeyDown={handleKeyDown} onChange={handleChangeTodo} value={todo} type="text" placeholder="Add a new todo" />
+      <button className="todo-button" onClick={onAddTodo}>Add</button>
+    </div>
+  )
+}
 
+function TodoList({ todos }: { todos: string[] }) {
+  return (
+    <ul className="todo-list">
+      {todos.map((todo) => (
+        <li className="todo-item" key={todo}>{todo}</li>
+      ))}
+    </ul>
+  )
+}
+
+function App() {
+  const [todos, setTodos] = useState<string[]>([])
+  const [todo, setTodo] = useState('')
+
+  const handleAddTodo = () => {
+    setTodos([...todos, todo])
+    setTodo('')
+  }
   return (
     <div className="app-container">
       <h1 className="app-title">TODO LIST</h1>
-      <div className="todo-container">
-        <input className="todo-input" onKeyDown={handleKeyDown} onChange={handleChangeTodo} value={todo} type="text" placeholder="Add a new todo" />
-        <button className="todo-button" onClick={handleAddTodo}>Add</button>
-      </div>
-      <ul className="todo-list">
-        {todos.map((todo) => (
-          <li className="todo-item" key={todo}>{todo}</li>
-        ))}
-      </ul>
+      <InputHeader onAddTodo={handleAddTodo} setTodo={setTodo} todo={todo} />
+
+      <TodoList todos={todos} />
     </div>
   )
 }
