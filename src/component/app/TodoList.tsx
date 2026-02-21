8444fb2612ad96e1958a2646e5f7c696c85eb675
sangrim shin <sangrimshin2020@gmail.com>
2026-02-21 22:37:01 +0900

    [refactor]create TodoTitle component

--- src/component/app/TodoList.tsx
+++ src/component/app/TodoList.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
 import type { Todo } from '../../type/Todo';
 import './TodoList.css';
+import TodoTitle from './todoList/TodoTitle';
 
 export default function TodoList({
     todoList,
@@ -13,30 +13,6 @@ export default function TodoList({
     handleCheck: (index: number) => void;
     handleEdit: (id: number, text: string) => void;
 }) {
-    const [editingId, setEditingId] = useState<number | null>(null);
-    const [editingText, setEditingText] = useState<string>('');
-    // 수정 버튼 클릭 시 수정 모달 창 띄우기
-    const startEdit = (id: number) => {
-        setEditingId(id);
-        setEditingText(todoList.find((todo) => todo.id === id)?.text || '');
-    };
-
-    const stopEdit = (id: number, text: string) => {
-        console.log('stopEdit', id, text);
-        setEditingId(null);
-        handleEdit(id, text);
-    };
-
-    const handleKeyDown = (
-        id: number,
-        e: React.KeyboardEvent<HTMLInputElement>,
-    ) => {
-        if (e.key === 'Enter') {
-            stopEdit(id, editingText);
-        } else if (e.key === 'Escape') {
-            setEditingId(null);
-        }
-    };
     return (
         <ul className="todo-list">
             {todoList.map((todo) => (
@@ -46,29 +22,16 @@ export default function TodoList({
                         className="todo-checkbox"
                         type="checkbox"
                     />
-                    {editingId === todo.id ? (
-                        <input
-                            className="todo-title"
-                            type="text"
-                            value={editingText}
-                            autoFocus
-                            onChange={(e) => setEditingText(e.target.value)}
-                            onBlur={(e) => stopEdit(todo.id, e.target.value)}
-                            onKeyDown={(e) => handleKeyDown(todo.id, e)}
-                        />
-                    ) : (
-                        <div
-                            className={`todo-title${todo.completed ? ' completed' : ''}`}
-                            onClick={() => startEdit(todo.id)}
-                        >
-                            {todo.text}
-                        </div>
-                    )}
+                    <TodoTitle
+                        todo={todo}
+                        todoList={todoList}
+                        handleEdit={handleEdit}
+                    />
                     <button
                         className="todo-delete-button"
                         onClick={() => handleDelete(todo.id)}
                     >
-                        삭제
+                        Del
                     </button>
                 </li>
             ))}
---
+++ src/component/app/todoList/TodoTitle.tsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import type { Todo } from '../../../type/Todo';
+import './TodoTitle.css';
+
+export default function TodoTitle({
+    todo,
+    todoList,
+    handleEdit,
+}: {
+    todo: Todo;
+    todoList: Todo[];
+    handleEdit: (id: number, text: string) => void;
+}) {
+    const [editingId, setEditingId] = useState<number | null>(null);
+    const [editingText, setEditingText] = useState<string>('');
+
+    const stopEdit = (id: number, text: string) => {
+        console.log('stopEdit', id, text);
+        setEditingId(null);
+        handleEdit(id, text);
+    };
+
+    // 수정 버튼 클릭 시 수정 모달 창 띄우기
+    const startEdit = (id: number) => {
+        setEditingId(id);
+        setEditingText(todoList.find((todo) => todo.id === id)?.text || '');
+    };
+    const handleKeyDown = (
+        id: number,
+        e: React.KeyboardEvent<HTMLInputElement>,
+    ) => {
+        if (e.key === 'Enter') {
+            stopEdit(id, editingText);
+        } else if (e.key === 'Escape') {
+            setEditingId(null);
+        }
+    };
+
+    return editingId === todo.id ? (
+        <input
+            className="todo-title"
+            type="text"
+            value={editingText}
+            autoFocus
+            onChange={(e) => setEditingText(e.target.value)}
+            onBlur={(e) => stopEdit(todo.id, e.target.value)}
+            onKeyDown={(e) => handleKeyDown(todo.id, e)}
+        />
+    ) : (
+        <div
+            className={`todo-title${todo.completed ? ' completed' : ''}`}
+            onClick={() => startEdit(todo.id)}
+        >
+            {todo.text}
+        </div>
+    );
+}
